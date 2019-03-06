263dc52bc98cc39cd54175a5fcc67a4e4b7955c6
Mario Fragoso Garcia <wbtbwb.extrem@gmail.com>
2019-03-05 22:35:35 -0600

    agregado archivo modal.js que olvide agregar en el commit anterior

--- php/transaccion.php
+++ php/transaccion.php
@@ -10,27 +10,26 @@
 //	$transaccion=$_POST['valorEliminar'];
 //	$sqlInsertarEtiqueta= "INSERT INTO herramientas VALUES (NULL, '".$descripcion."', '".$codigo."', '".$valorCategoria."')";
 //	$aplicarTransaccion= $mysqli->query($sqlInsertarEtiqueta);
-$table="<table  border=\"1\">
+$table = "<table  border=\"1\">
   <tr><th>Mensaje</th></tr>
   <tr><td>Hola soy un mensaje</td></tr>
   </table>";
 
-        $email=$_POST['email'];
-        $pass=$_POST['pass'];
-         if ($email=='mario@gmail.com' and $pass == '123'){
-             $data = array(
-                 'tipo'=>1,
-                 'msg'=>"bienvenido");
-             echo json_encode($data);
+$email = $_POST['email'];
+$pass = $_POST['pass'];
+if ($email == 'mario@gmail.com' and $pass == '123') {
+    $data = array(
+        'tipo' => 1,
+        'msg' => "bienvenido");
+    echo json_encode($data);
 
 
-         }
-         else if ($email==''||$pass==''){
-             $data = array(
-                 'tipo'=>2,
-                 'msg'=>"Ingresa Datos");
-             echo json_encode($data);
+} else if ($email == '' || $pass == '') {
+    $data = array(
+        'tipo' => 2,
+        'msg' => "Ingresa Datos");
+    echo json_encode($data);
 
-         }
+}
 
 ?>
