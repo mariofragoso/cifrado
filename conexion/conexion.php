dc480873e12bc7904bd550e4d112666f68ac5053
Mario Fragoso Garcia <wbtbwb.extrem@gmail.com>
2019-03-05 22:21:49 -0600

    Base de datos bien

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,47 +1,13 @@
 <?php
+try {
+    $base = new PDO("mysql:host=127.0.0.1;dbname=cifrado", "root", "Wbtbwb1217*");
+    echo "Conexion OK";
 
+} catch (Exception $e) {
+    die("Error:" . $e->getMessage());
 
-class Conectar {
-
-    protected $dbh;
-
-    protected function conexion(){
-
-
-        try {
-
-            $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=cifrado","root","Wbtbwb1217*");
-
-            return $conectar;
-            console.log("Conexion existosa");
-
-        } catch (Exception $e) {
-
-
-            print "¡Error!: " . $e->getMessage() . "<br/>";
-            die();
-
-
-        }
-
-
-    } //cierre de llave de la function conexion()
-
-
-    public function set_names(){
-
-        return $this->dbh->query("SET NAMES 'utf8'");
-    }
-
-
-    public function ruta(){
-
-        return "http://localhost/cifrado/";
-
-    }
-
-
-
-}//cierre de llave conectar
+} finally {
+    $base = null;
+}
 
 ?>
